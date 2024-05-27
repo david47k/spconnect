ff4e31d8000e9a40d07ee94f74f783273053bbf0
david47k <67101285+david47k@users.noreply.github.com>
2024-05-27 15:04:00 +1000

    Add welcome message and basic help.

--- spconnect.c
+++ spconnect.c
@@ -1,65 +1,21 @@
-/*
-# spconnect
+// spconnect: Connects to a serial port from a Windows Terminal/Console.
+// Copyright 2024 David Atkinson. MIT License. 
+// See README.md for more information.
+// Available from https://github.com/david47k/spconnect/
 
-Connects to a serial port from a Windows Terminal/Console.
-Copyright 2024 David Atkinson. MIT License. 
-Available from https://github.com/david47k/spconnect/
-
-## About
-
-This program connects to a serial port from a Windows Console/Terminal.
-
-It's a basic program, designed to be used directly from Windows Terminal, as an
-alternative to e.g. PuTTY. It is tested on (and designed to work on) Windows 10.
-
-## Using the program
-
-### Configuring the serial port
-
-You can specify baud rate etc. by first using the windows built-in `mode`
-command. Running 'mode' by itself will give you a list of serial ports.
-Common baud rates: 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600,
-115200, 230400, 460800, 921600. e.g.:
-
-`mode com1 baud=115200 parity=n data=8 stop=1 to=off xon=off odsr=off octs=off dtr=on rts=on`
-
-or
-
-`mode com1 115200,n,8,1`
-
-### Starting the program
-
-Start this program with the serial port as an argument, along with any options. e.g.:
-
-`spconnect com1 -W 10000`
-
-### Options
-
-```
--L       --local-echo          Enable local echo of characters typed.
--S       --system-codepage     Use system codepage instead of UTF-8.
--R       --replace-cr          Replace input CR (\r) with newline (\n).
--D       --disable-vt          Disable sending and receiving of virtual terminal (VT) codes.
--W 100   --write-timeout 100   Serial port write timeout, in milliseconds. Default 1000.
-```
-
-### Quitting
-
-Use `Ctrl-F10` to quit.
-
-### Using a different codepage
-
-The default is to use UTF-8 for console input and output. You can use the system
-codepage instead by using the `-S` option. You can check the system codepage 
-and change it using the the windows built-in `mode con cp` command. e.g.:
-
-`mode con cp select=1251`
-
-### Disable VT processing (raw mode)
-
-The default is to process VT commands from both the keyboard and the serial 
-port. You can disable VT processing (essentially a raw mode) using `-D`.
-*/
+const char* HELP_MSG =
+    "Usage: spconnect <PORT> [OPTIONS]\n"
+    "e.g.:  spconnect com1 -W 10000\n"
+    "\n"
+    "Options:\n"
+    "  -L      --local-echo         Enable local echo of characters typed.\n"
+    "  -S      --system-codepage    Use system codepage instead of UTF-8.\n"
+    "  -R      --replace-cr         Replace input CR (\\r) with newline (\\n).\n"
+    "  -D      --disable-vt         Disable sending and receiving of virtual terminal (VT) codes.\n"
+    "  -W 100  --write-timeout 100  Serial port write timeout, in milliseconds. Default 1000.\n"
+    "\n"
+    "Use Ctrl-F10 to quit.\n"
+    "See README.md for more information.\n";
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -367,6 +323,10 @@ int main(int argc, char* argv[]) {
             else if (strcmp(arg, "--debug-input") == 0) {
                 DebugInput = true;
             }
+            else if (strcmp(arg, "--help") == 0 || strcmp(arg, "-h") == 0) {
+                fprintf(stderr, "\n%s", HELP_MSG);
+                exit(0);
+            }
             else if (strcmp(arg, "--write-timeout") == 0 || strcmp(arg, "-w") == 0) {
                 // check we have a follow-up number
                 if((i+1) >= argc) {
@@ -385,7 +345,7 @@ int main(int argc, char* argv[]) {
 
     // Check that we have a serial port
     if (sp_s[0] == 0) {
-        fprintf(stderr, "\nPlease specify a serial port. e.g. spconnect com1\n");
+        fprintf(stderr, "\nPlease specify a serial port. e.g. 'spconnect com1'. For help, run 'spconnect --help'.\n");
         exit(1);
     }
 
@@ -395,7 +355,7 @@ int main(int argc, char* argv[]) {
     HANDLE h_port   = InitPort(sp_s);
 
     // Display a welcome message.
-    printf("Connecting to %s. Press Ctrl-F10 to quit.\n");
+    fprintf(stderr, "Connecting to %s. Press Ctrl-F10 to quit.\n", sp_s);
 
     // Main loop. Copy the data from stdin to the serial port, and from the serial port to stdout.
     while (1) {        
